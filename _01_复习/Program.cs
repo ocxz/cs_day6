dd69a5f521dffd1abb6aefa9a08aca0fa74f9e48
ocxz <2252779530@qq.com>
2019-07-11 11:12:56 +0800

    添加项目文件。

---
+++ _01_复习/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_复习
+{
+    class Program
+    {
+        // 声明一个Gender枚举，用来存储性别
+        public enum Gender
+        {
+            男,
+            女
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            /**
+             *   复习内容：常量 枚举 结构 数组
+             *   常量声明方式：const int number = 100;
+             * 
+             */
+        }
+    }
+}
